083a91af2a58536af0107006136097c8b9e7607d
pakfron <pakpoom.poom@hotmail.com>
2024-02-17 23:01:31 +0700

    add route

--- app/main.go
+++ app/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"pp-model-shop-backend/modules/server"
-	"pp-model-shop-backend/modules/users/controllers"
+	"pp-model-shop-backend/routes"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -12,6 +12,8 @@ func main() {
 	server.CreateDataBase()
 	r := gin.Default()
 	r.Use(cors.Default())
-	r.POST("/register", controllers.Register)
+	router := r.Group("")
+	routes.AddRoutes(router)
+	// r.POST("/register", controllers.Register)
 	r.Run("localhost:8008") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
